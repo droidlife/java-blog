a1520890a2c0159ec980be17a2fabbc2d89cdd8d
Ankur Jain <ankurj630@gmail.com>
2016-01-22 23:58:29 +0530

    s

--- src/main/java/com/spring/entity/User.java
+++ src/main/java/com/spring/entity/User.java
@@ -10,9 +10,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name="user_user")
 public class User {
 
 	@Id
